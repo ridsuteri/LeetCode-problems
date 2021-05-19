7591939bc87e27262f5ad6dc58830dd37987938f
Riddhi Suteri <ridsuteri@gmail.com>
2021-05-19 09:43:10 +0530

    problem 55

---
+++ jumpGame.java
@@ -0,0 +1,17 @@
+public class jumpGame {
+    //     valley peak approch
+    public boolean canJump(int[] nums) {
+        
+        int reachable = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            if(i > reachable )
+                return false;
+            
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        
+        return true;
+    }
+}
