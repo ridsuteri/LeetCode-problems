5e094e83290af7611d0f3ee1e6e85f179b4dec20
Riddhi Suteri <ridsuteri@gmail.com>
2021-06-14 22:59:34 +0530

    problem 83

---
+++ removeDuplicateLL.java
@@ -0,0 +1,16 @@
+class removeDuplicateLL {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+ 
+        ListNode temp = head;
+        while(temp != null){
+            if(temp.next != null && temp.val == temp.next.val)
+                temp.next = temp.next.next;
+            
+            else
+                temp = temp.next;
+        }
+        return head;
+    }
+}
